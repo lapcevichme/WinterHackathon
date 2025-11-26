01b9f73652691551e96072172c9b9b2cfb7e1a52
Michael Lapcevich <149459288+lapcevichme@users.noreply.github.com>
2025-11-26 19:05:07 +0700

    feat(mobile): Add Hilt for dependency injection

---
+++ mobile/app/src/main/java/com/lapcevichme/winterhackathon/AppModule.kt
@@ -0,0 +1,9 @@
+package com.lapcevichme.winterhackathon
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {}
---
+++ mobile/app/src/main/java/com/lapcevichme/winterhackathon/WinterHackathonApplication.kt
@@ -0,0 +1,7 @@
+package com.lapcevichme.winterhackathon
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class WinterHackathonApplication : Application()
